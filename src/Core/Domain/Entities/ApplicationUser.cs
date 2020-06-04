431ad96c267e35f6238b0da03459035b1c11795c
Mati <mwiczuk99@gmail.com>
2020-06-04 13:23:38 +0200

    Add some model entities

--- src/Core/Domain/Entities/ApplicationUser.cs
+++ src/Core/Domain/Entities/ApplicationUser.cs
@@ -5,6 +5,9 @@ namespace Domain.Entities
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Pet> Pets { get; set; }
         public List<Token> refreshTokens = new List<Token>();
     }
 }
---
+++ src/Core/Domain/Entities/Pet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public ICollection<string> AppointmentsHistory { get; set; }
+
+        public int OwnerId { get; set; }
+        [ForeignKey("OwnerId")] 
+        public virtual ApplicationUser Owner { get; set; }
+    }
+}
