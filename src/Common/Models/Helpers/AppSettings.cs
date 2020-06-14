db53aa3f97e158771003e8a2c5d184737c3ca0fd
MateuszW <47317247+MateuszW99@users.noreply.github.com>
2020-06-14 19:20:42 +0200

    Revert "Feature/login update"

--- src/Common/Models/Helpers/AppSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Models.Helpers
-{
-    public class AppSettings
-    {
-        public string Secret { get; set; }
-        public string ValidIssuer { get; set; }
-        public string ValidAudience { get; set; }
-    }
-}
