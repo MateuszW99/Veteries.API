88a1d206a28581f186c0cfb8a6d13ec5101c9735
Mati <mwiczuk99@gmail.com>
2020-10-16 13:02:06 +0200

    Add httpService and change claim from id to NameIdentifier

---
+++ src/Common/Services/Externals/HttpService.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Services.Abstractions;
+using Microsoft.AspNetCore.Http;
+
+namespace Services.Externals
+{
+    public class HttpService : IHttpService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HttpService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetUserId()
+        {
+            if (_httpContextAccessor?.HttpContext.User == null)
+            {
+                return string.Empty;
+            }
+
+            return _httpContextAccessor.HttpContext.User.Claims
+                .Single(x => x.Type == ClaimTypes.NameIdentifier).Value;
+        }
+    }
+}
--- src/Feature/User/Internals/IdentityService.cs
+++ src/Feature/User/Internals/IdentityService.cs
@@ -158,7 +158,7 @@ namespace User.Internals
                 new Claim(JwtRegisteredClaimNames.Sub, user.Email),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
                 new Claim(JwtRegisteredClaimNames.Email, user.Email),
-                new Claim("id", user.Id)
+                new Claim(ClaimTypes.NameIdentifier, user.Id)
             };
 
             var userClaims = await _userManager.GetClaimsAsync(user);
