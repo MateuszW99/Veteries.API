4df17ef3c259067df76011bdd4374e4087c1ca81
MateuszW <mwiczuk99@gmail.com>
2020-10-30 21:43:48 +0100

    Add animal controller test class

---
+++ test/IntegrationTests/Endpoints/AnimalControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace Veteries.IntegrationTests.Endpoints
+{
+    public class AnimalControllerTests : IntegrationTest
+    {
+        [Fact]
+        public async Task Task_GetAll_WithNoAnimals_ReturnsEmptyResponse()
+        {
+            await AuthenticateAsync();
+
+            var response = await TestClient.GetAsync("api/Animals");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await response.Content.ReadAsAsync<List<Animal>>()).Should().BeEmpty();
+        }
+    }
+}
