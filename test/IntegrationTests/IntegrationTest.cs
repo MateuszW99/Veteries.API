892ddae1284f516564562bf7dda6ff417c6fc0ae
MateuszW <mwiczuk99@gmail.com>
2020-10-30 21:43:34 +0100

    Add testing solution

---
+++ test/IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Persistence.Domain;
+using User.Models.Commands;
+using User.Models.Results;
+using Veteries.API;
+
+namespace Veteries.IntegrationTests
+{
+    public class IntegrationTest : IDisposable
+    {
+        protected readonly HttpClient TestClient;
+        private readonly IServiceProvider _serviceProvider;
+
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(DomainDbContext));
+                        services.AddDbContext<DomainDbContext>(options => { options.UseInMemoryDatabase("TestDb"); });
+                    });
+                });
+
+            _serviceProvider = appFactory.Services;
+            TestClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", await GetJwtAsync());
+        }
+
+        private async Task<string> GetJwtAsync()
+        {
+            var response = await TestClient.PostAsJsonAsync("api/auth/register", new RegisterUserCommand
+            {
+                Email = "test@test.com",
+                Password = "test1234!",
+                RepeatedPassword = "test1234!"
+            });
+
+            var registrationResponse = await response.Content.ReadAsAsync<IdentityResult>();
+            return registrationResponse.Token;
+        }
+
+        public void Dispose()
+        {
+            using var serviceScope = _serviceProvider.CreateScope();
+            var context = serviceScope.ServiceProvider.GetService<DomainDbContext>();
+            context.Database.EnsureDeleted();
+        }
+    }
+}
