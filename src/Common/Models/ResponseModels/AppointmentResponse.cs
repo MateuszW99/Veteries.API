46f8c63580ee28321b5b4e2de672490b96468880
MateuszW <mwiczuk99@gmail.com>
2020-11-01 14:08:27 +0100

    Add Animal & Appointment DTOs

---
+++ src/Common/Models/ResponseModels/AppointmentResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Models.ResponseModels
+{
+    public class AppointmentResponse
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public AnimalResponse Animal { get; set; }
+    }
+}
